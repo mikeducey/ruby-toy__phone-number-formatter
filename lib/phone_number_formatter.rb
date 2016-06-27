f03b2eb01d7d6d48439cf9339b42a488797281e6
Michael <michaeljducey@gmail.com>
2016-06-27 03:38:41 +0000

    Submitting work, all tests passed for phone-number-format

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,12 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
+    phone_number_str = phone_number_str.gsub(/[ ]/, "")
+    phone_number_str = phone_number_str.gsub(/[.]/, "")
+    phone_number_str = phone_number_str.gsub(/[-]/, "")
+    phone_number_str.insert(0, "(")
+    phone_number_str.insert(4, ")")
+    phone_number_str.insert(5, " ")
+    phone_number_str.insert(9, "-")
   return phone_number_str
 end
